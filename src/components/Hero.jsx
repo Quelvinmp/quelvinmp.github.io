e4a27cec84f48d1a4120805856bb5690fdf476b2
Quelvinmp <quelvinmp@gmail.com>
2023-06-04 15:10:50 -0300

    fix: change hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,10 @@ export default function Hero() {
       <div className="flex-col flex-1 gap-4 sm:gap-10 lg:gap-[1rem] hero-content lg:flex-row-reverse w-full lg:justify-around justify-center" data-aos="zoom-in" data-aos-duration="1000">
         <img src={perfil} className="max-w-xs rounded-lg sm:max-w-sm" />
         <div className="flex flex-col items-center justify-center gap-2 sm:gap-0 " >
-          <h1 className="text-3xl font-bold leading-tight text-center sm:text-5xl">Olá, me chamo <span className="text-primary">Quelvin</span>!</h1>
-          <p className="max-w-lg text-sm text-center sm:text-base sm:pt-6">Comecei a estudar programação no final de 2021 e desde então não me imagino trabalhando com outra coisa se não nessa área, que tem uma incrível capacidade de resolver tanto pequenos problemas do dia a dia como o de mega empresas!</p>
+          <h1 className="text-3xl font-bold leading-tight text-center sm:text-5xl">Olá, me chamo <span className="text-primary">Quelvin</span>!<br/>
+          <span className="text-xl ">Desenvolvedor Front-End</span>
+          </h1>
+          <p className="max-w-lg text-sm text-center sm:text-base sm:pt-6">Desde 2022 me aprofundando e aprendendo novas tecnologias para solucionar os problemas das empresas.</p>
           <a href="#contact" className="btn btn-link">Posso resolver o da sua?</a>
         </div>
       </div>
