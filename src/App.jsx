9ed2bb56837b7a464234b476bafbbf5246db76f2
Quelvinmp <quelvinmp@gmail.com>
2024-01-25 13:15:10 -0300

    feat: test - amazon affiliate

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import Portifolio from './pages/Portifolio'
 import NotFound from './pages/NotFound'
 import Feedback from './pages/Feedback'
+import Test from './pages/Test';
 
 
 function App() {
 
   return (
     <Routes>
-      <Route exact path="/" element={<Portifolio/>}/>      
-      <Route path="/feedback" element={<Feedback/>}/>      
-      <Route path="*" element={<NotFound/>}/>      
+      <Route exact path="/" element={<Portifolio />} />
+      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
